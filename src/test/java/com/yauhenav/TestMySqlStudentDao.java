6d72063ac5d7942453285e5a8cb4d8b12545dfcc
yauhenav <enavitski@gmail.com>
2017-08-02 23:18:09 +0300

    the only method improved

--- src/test/java/com/yauhenav/TestMySqlStudentDao.java
+++ src/test/java/com/yauhenav/TestMySqlStudentDao.java
@@ -2,15 +2,14 @@ package com.yauhenav;
 
 import static org.junit.Assert.*;
 
-import com.yauhenav.logic.exception.DaoException;
-import com.yauhenav.logic.mysql.MySqlDaoFactory;
-import com.yauhenav.logic.mysql.MySqlStudentDao;
-import com.yauhenav.logic.dao.*;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.yauhenav.logic.exception.DaoException;
+import com.yauhenav.logic.mysql.*;
 import com.yauhenav.logic.dto.*;
 
-import java.sql.ResultSet;
 import java.sql.*;
 
 /**
@@ -18,20 +17,36 @@ import java.sql.*;
  */
 public class TestMySqlStudentDao {
 
-    @Before
-    public void connectToDataBase() throws SQLException {
+    public Connection connection;
+    String pathDB = "/testDataBase.properties";
+    MySqlStudentDao testMSSD;
 
+    @Before
+    public void connectToDataBase() throws DaoException {
+        try {
+            MySqlDaoFactory testMSDF = new MySqlDaoFactory(pathDB);
+            connection = testMSDF.connection;
+            testMSSD = new MySqlStudentDao(connection, pathDB);
+        } catch (DaoException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    @After
+    public void closeConnection() throws SQLException {
+        try {
+            connection.close();
+        } catch (SQLException exc) {
+            exc.printStackTrace();
+        }
     }
 
     @Test
     public void TestCreateStudentMethod () throws SQLException, DaoException {
         try {
-            MySqlDaoFactory testMSDF = new MySqlDaoFactory("/testDataBase.properties");
-            StudentDao testInterStudentDao = testMSDF.getStudentDao();
-            Student testStudentPush = new Student (11, "Sherlock", "Holmes" );
-            testInterStudentDao.create(testStudentPush);
+            Student testStudentPush = new Student (12, "Sherlock", "Holmes" );
+            testMSSD.create(testStudentPush);
 
-            Connection connection = testMSDF.connection;
             PreparedStatement ps = connection.prepareStatement("SELECT ID, NAME, SURNAME FROM testDataBase.STUDENT WHERE ID = ?");
             ps.setInt(1, testStudentPush.getId());
             ResultSet rs = ps.executeQuery();
@@ -40,6 +55,7 @@ public class TestMySqlStudentDao {
             testStudentPop.setId(rs.getInt("ID"));
             testStudentPop.setName(rs.getString("NAME"));
             testStudentPop.setSurname(rs.getString("SURNAME"));
+
             assertEquals (testStudentPush.toString(), testStudentPop.toString());
         } catch (DaoException exc) {
             exc.printStackTrace();
